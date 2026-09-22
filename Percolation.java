a2e1da4c64eba8b92dc4cdfac1335e503a7443fd
asaxena27 <asaxena27@jesuitmail.org>
2026-09-22 13:47:55 -0700

    yes

--- Percolation.java
+++ Percolation.java
@@ -1,9 +1,45 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private WeightedQuickUnionUF[] uf;
+    private boolean openSites[];
+    private int n;
 
-
-    public static void main(String[] args) {
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n)
+    {
+        if(n<=0)
+        {
+            throw new IllegalArgumentException("n must be greater than 0");
+        }
+        this.n=n;
+        this.uf= new WeightedQuickUnionUF[n*n+2];
+        this.openSites= new boolean[n* n+2];
+        openSites[0]=true;
+        openSites[n*n+2]=true;
 
     }
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col)
+    {
+        if(!openSites[(n-1)*row;
+    }
+
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col)
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col)
+
+    // returns the number of open sites
+    public int numberOfOpenSites()
+
+    // does the system percolate?
+    public boolean percolates()
+
+    // test client (optional)
+    public static void main(String[] args)
 }
+
