3f87b8005530c3bf2fab5e4be4d501cc4fef6269
asaxena27 <asaxena27@jesuitmail.org>
2026-09-25 13:37:22 -0700

    yes

--- PercolationStats.java
+++ PercolationStats.java
@@ -4,7 +4,20 @@ import edu.princeton.cs.algs4.StdStats;
 public class PercolationStats {
 
 
-    public static void main(String[] args) {
+    public PercolationStats(int n, int trials)
 
-    }
+    // sample mean of percolation threshold
+    public double mean()
+
+    // sample standard deviation of percolation threshold
+    public double stddev()
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo()
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi()
+
+    // test client (see below)
+    public static void main(String[] args)
 }
